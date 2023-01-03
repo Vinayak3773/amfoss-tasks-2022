6da4b2ca1ebc947c987775eb1fa77688c5f37428
Vinayak3773 <vinayakcm2003@gmail.com>
2023-01-03 21:41:09 +0530

    update

--- task-04/bot.py
+++ task-04/bot.py
@@ -4,11 +4,18 @@ import requests
 import json
 import csv
 
+
+
 # TODO: 1.1 Get your environment variables 
 yourkey ="60893b7e"
 bot_id = "5836453032:AAH09MrKlGxNnU8Ki6OELG9B8gp3PuX4GsE"
 
+
+
 bot = telebot.TeleBot(bot_id)
+
+
+
 
 @bot.message_handler(commands=['start', 'hello'])
 def greet(message):
@@ -16,6 +23,9 @@ def greet(message):
     botRunning = True
     bot.reply_to(
         message, 'Hello there! I am a bot that will show mov information for you and export it in a CSV file.\n\n')
+
+
+
     
 @bot.message_handler(commands=['stop', 'bye'])
 def goodbye(message):
@@ -25,9 +35,16 @@ def goodbye(message):
     
 
 
+
+
+
 @bot.message_handler(func=lambda message: botRunning, commands=['help'])
 def helpProvider(message):
     bot.reply_to(message, '1.0 You can use \"/movie MOVIE_NAME\" command to get the details of a particular mov. For eg: \"/movie The Shawshank Redemption\"\n\n2.0. You can use \"/export\" command to export all the mov data in CSV format.\n\n3.0. You can use \"/stop\" or the command \"/bye\" to stop the bot.')
+
+
+
+
 
 
 @bot.message_handler(func=lambda message: botRunning, commands=['movie'])
@@ -36,13 +53,16 @@ def getMovie(message):
     mov = message.text
     mov = mov.replace("/movie","")
     response = requests.get( f"http://www.omdbapi.com/?apikey=bcd3f769&t={mov}")
-    mov_info=response.json()
-    print(json.dumps(mov_info,indent = 4))
-    bot.reply_to(message,f"{mov_info['Poster']} \nMovie Name: {mov_info['Title']} \nImdb Rating: {mov_info['imbdRating']} \nYear: {mov_info['Year']} \nReleased: {mov_info['Released']}")
-    bot.send_photo(message,{mov_info['Poster']})
+    mov_data=response.json()
+    print(json.dumps(mov_data,indent = 4))
+    bot.reply_to(message,f"{mov_data['Poster']} \nMovie Name: {mov_data['Title']} \nYear: {mov_data['Year']} \nReleased: {mov_data['Released']} \nImdb Rating: {mov_data['imdbRating']}")
+    bot.send_photo(message,{mov_data['Poster']})
     with open('mov_data.csv', 'a') as f:
         writer = csv.writer(f)
-        writer.writerow([mov_info['Title'],mov_info['imbdRating'],mov_info['Year'],mov_info['Released']])
+        writer.writerow([mov_data['Title'],mov_data['Year'],mov_data['Released'],mov_data['imdbRating']])
+
+
+
 
 
   
@@ -51,9 +71,13 @@ def getList(message):
     bot.reply_to(message, 'Generating file...')
     chat_id = message.chat.id 
     print()
-    mov_info=open('mov_data.csv','rb')
-    bot.send_document(chat_id,mov_info)
+    mov_data=open('mov_data.csv','rb')
+    bot.send_document(chat_id,mov_data)
     
+
+
+
+
 
 @bot.message_handler(func=lambda message: botRunning)
 def default(message):
